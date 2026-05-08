1b8ffe3a4e9191a9a91dbe9145296d68d5c0e5ac
krishnakasaudhan0 <kasaudhankrishna05@gmail.com>
2026-05-08 14:05:32 +0000

    feat: add websocket telemetry listener

--- src/socket.js
+++ src/socket.js
@@ -2,3 +2,5 @@
 // Git contribution logic update at: 2026-05-07T06:03:44.000Z [hash: 9rp6ik]
 
 // Git contribution logic update at: 2026-05-08T13:15:03.000Z [hash: ybrnb]
+
+// Git contribution logic update at: 2026-05-08T14:05:32.000Z [hash: k60ph]
